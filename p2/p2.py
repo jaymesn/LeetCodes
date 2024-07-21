83784d242cc2424970006880e8d496d7d42f5c3c
jaymesnjjclyke <jjclyke@gmail.com>
2024-07-21 19:49:57 -0400

    Remeber python as one-liner if statements

--- p2/p2.py
+++ p2/p2.py
@@ -13,8 +13,12 @@ class ListNode:
             current = ListNode(array[i])
             prev.next = current
             prev = current
-            
-        return head
+
+        self.val = head.val
+        self.next = head.next
+
+        return self
+
 
     def fmtNodes(self):
         head = ListNode(self.val,self.next)
@@ -30,9 +34,60 @@ class ListNode:
         arr.append(current.val)
 
 
-
-        return arr
+        print(arr)
         
+        return self
+
+class Solution(object):
+    def addTwoNumbers(self, l1:ListNode, l2:ListNode) -> ListNode:
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """    
+        carry = 0
+        head = ListNode()
+        cur = head
+
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            # new digit calculated and inserted
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+
+            # update the ptrs
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return head.next
+             
+            
+            
+
+#ksdfjsldj
+
+
+
+
+tests = [ 
+    [[2,4,3],[5,6,4]],
+    [[0],[0]],
+    [[9,9,9,9,9,9,9],[9,9,9,9]]
+]
+solution = Solution().addTwoNumbers
+
+for item in tests:
+    solution(
+        ListNode().convertArray(item[0]),
+        ListNode().convertArray(item[1])
+    ).fmtNodes()
+
+    
 
 '''
 Example 1:
@@ -51,7 +106,3 @@ Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
 Output: [8,9,9,9,0,0,0,1]
 
 '''
-
-newNode = ListNode(9)
-newList = ListNode().convertArray([2,3,4])
-print(newList.fmtNodes())
